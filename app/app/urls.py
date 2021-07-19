91156bd8694814e42c5dfa0139780357079ef795
tejas-trivedi <500077185@stu.upes.ac.in>
2021-07-19 13:41:55 +0530

    requirements

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 urlpatterns = [
